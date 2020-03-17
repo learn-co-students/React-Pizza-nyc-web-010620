c533635386ce7b36b14e1ef871bbff9204d5b917
edhernandez04 <mchh.004@gmail.com>
2020-03-17 14:36:10 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,80 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+
+  state = {
+    pizzas: [],
+    currentPizza: {
+      id: '',
+      topping: '',
+      size: '',
+      vegetarian: false
+    }
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/pizzas")
+      .then(resp => resp.json())
+      .then(pizzas => this.setState({pizzas}))
+  }
+
+  handleClick = (pizza) => {
+    console.log(pizza)
+    this.setState({
+      ...this.state.pizzas,
+      currentPizza: {
+        id: pizza.id,
+        topping: pizza.topping,
+        size: pizza.size,
+        vegetarian: pizza.vegetarian
+      }
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state.pizzas,
+      currentPizza: {
+        ...this.state.currentPizza,
+          [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  handleVeggieChange = (e) => {
+    let status = e.target.value == "Vegetarian" ? true : false
+    this.setState({
+      ...this.state.pizzas,
+      currentPizza: {
+        ...this.state.currentPizza,
+          vegetarian: status
+      }
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:3000/pizzas/${this.state.currentPizza.id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json;charset=utf-8' },
+      body: JSON.stringify( this.state.currentPizza ) })
+      .then(response => response.json())
+      .then(pizza => {
+        let thisPizzaIndex = this.state.pizzas.findIndex(za => za.id === pizza.id)
+        let copyArr = [...this.state.pizzas]
+        copyArr[thisPizzaIndex] = pizza
+        this.setState({
+          pizzas: copyArr
+        })
+      })
+  }
+
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm handleSubmit={this.handleSubmit} pizza={this.state.currentPizza} handleVeggieChange={this.handleVeggieChange} handleChange={this.handleChange}/>
+        <PizzaList pizzas={this.state.pizzas} handleClick={this.handleClick}/>
       </Fragment>
     );
   }
--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,16 @@
 import React from "react"
 
-const PizzaForm = () => {
+export default class PizzaForm extends React.Component {
+
+  render(){
   return(
+    <form onSubmit={this.props.handleSubmit}>
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+            <input onChange={this.props.handleChange} name="topping" type="text" className="form-control" placeholder="Pizza Topping" value={this.props.pizza.topping} />
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select onChange={this.props.handleChange} value={this.props.pizza.size} name="size" className="form-control">
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,24 +18,23 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input className="form-check-input" onChange={this.props.handleVeggieChange} type="radio" name="vegetarian" value="Vegetarian" checked={this.props.pizza.vegetarian}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input className="form-check-input" onChange={this.props.handleVeggieChange} name="vegetarian" type="radio" value="Not Vegetarian" checked={!this.props.pizza.vegetarian}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" onClick={() => console.log("this a submit")}>Submit</button>
         </div>
       </div>
-
+      </form>
   )
+  }
 }
-
-export default PizzaForm
